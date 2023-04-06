8479fbf31ee70400ea8902f33dda1f07a0838cb2
felipematheus1337 <lipehbr@gmail.com>
2023-04-06 10:10:33 -0300

    Criado package app, e funcionalidade de buscar por IP

---
+++ app/app.go
@@ -0,0 +1,46 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+// Gerar vai retornar a aplicação de linha de comando pronta para ser executada
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de Linha de comando"
+	app.Usage = "Busca IPs e Nomes de Servidor na Internet"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPS de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, err := net.LookupIP(host)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"linha-comando/app"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Ponto de partida")
+
+	aplicacao := app.Gerar()
+
+	if err := aplicacao.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+
 }
